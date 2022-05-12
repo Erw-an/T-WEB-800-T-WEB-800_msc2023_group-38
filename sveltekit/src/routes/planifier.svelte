4a2716d24a53a271fbc1e3875ec72a95a15b3e53
josephSaleon <joseph.saleonterras@gmail.com>
2022-05-12 14:31:30 +0200

    ✨ 🐛 fix navigation + add mapbox

--- sveltekit/src/routes/planifier.svelte
+++ sveltekit/src/routes/planifier.svelte
@@ -1,3 +1,36 @@
+<script lang="ts">
+	import Autocomplete from '@smui-extra/autocomplete';
+	import Card, { Content } from '@smui/card';
+	let fruits = ['Apple', 'Orange', 'Banana', 'Mango'];
+	let valueStart: string | undefined = undefined;
+	let valueEnd: string | undefined = undefined;
+	 
+	// var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+ 
+	// mapboxgl.accessToken = 'pk.eyJ1Ijoic3dlLWpvZWUiLCJhIjoiY2wzMnFlcXo1MDBsNTNjcXNpa3JrdzByZSJ9._H6XTxj4p2OFQ2DGMKENdw';
+	// 	var map = new mapboxgl.Map({
+	// 	container: 'map',
+	// 	style: 'mapbox://styles/mapbox/streets-v11'
+	// });
+	
+</script>
+
+<svelte:head>
+	<script src='https://api.mapbox.com/mapbox-gl-js/v2.8.2/mapbox-gl.js'></script>
+	<link href='https://api.mapbox.com/mapbox-gl-js/v2.8.2/mapbox-gl.css' rel='stylesheet' />
+	<div id='map' style='width: 400px; height: 300px;'></div>
+	<!-- MapBox -->
+	<link href='https://api.mapbox.com/mapbox-gl-js/v2.8.1/mapbox-gl.css' rel='stylesheet' />
+	<script src='https://api.mapbox.com/mapbox-gl-js/v2.8.1/mapbox-gl.js'></script>
+	<script>
+		mapboxgl.accessToken = 'pk.eyJ1Ijoic3dlLWpvZWUiLCJhIjoiY2wzMnFlcXo1MDBsNTNjcXNpa3JrdzByZSJ9._H6XTxj4p2OFQ2DGMKENdw';
+		var map = new mapboxgl.Map({
+		container: 'map',
+		style: 'mapbox://styles/mapbox/streets-v11'
+		});
+	</script>
+</svelte:head>
+
 <div class="columns margins">
 	<div class="container">
 		<div class="card-container">
@@ -15,21 +48,18 @@
 						bind:value={valueEnd}
 						label="Arriver"
 					/>
+					<div id='map'></div>
 				</div>
+				
 			</Card>
+			
 		</div>
 	</div>
 </div>
 
 
 
-<script lang="ts">
-	import Autocomplete from '@smui-extra/autocomplete';
-	import Card, { Content } from '@smui/card';
-	let fruits = ['Apple', 'Orange', 'Banana', 'Mango'];
-	let valueStart: string | undefined = undefined;
-	let valueEnd: string | undefined = undefined;
-</script>
+
 
 <style>
 	.card{
