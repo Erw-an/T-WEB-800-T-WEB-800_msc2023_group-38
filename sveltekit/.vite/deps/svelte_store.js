330eee83333e4cb42f841e1b585ca1522bba3dd2
josephSaleon <joseph.saleonterras@gmail.com>
2022-06-03 16:23:51 +0200

    ✨ add map

---
+++ sveltekit/.vite/deps/svelte_store.js
@@ -0,0 +1,15 @@
+import {
+  derived,
+  readable,
+  writable
+} from "./chunk-W55BWRTO.js";
+import {
+  get_store_value
+} from "./chunk-GFRLLTLI.js";
+export {
+  derived,
+  get_store_value as get,
+  readable,
+  writable
+};
+//# sourceMappingURL=svelte_store.js.map
--- sveltekit/src/routes/planifier.svelte
+++ sveltekit/src/routes/planifier.svelte
@@ -1,6 +1,7 @@
 <script type="ts">
 	import Autocomplete from '@smui-extra/autocomplete';
 	import {onMount} from 'svelte'
+	import '../app.css';
 	import Card, { Content } from '@smui/card';
 	let fruits = ['Apple', 'Orange', 'Banana', 'Mango'];
 	let valueStart: string | undefined = undefined;
@@ -9,13 +10,15 @@
 	import Map from "$lib/map/Map.svelte"
 	 
 	
-	import mapboxgl from 'mapbox-gl/dist/mapbox-gl'
+	import mapboxgl from 'mapbox-gl'
 	mapboxgl.accessToken = 'pk.eyJ1Ijoic3dlLWpvZWUiLCJhIjoiY2wzMnFlcXo1MDBsNTNjcXNpa3JrdzByZSJ9._H6XTxj4p2OFQ2DGMKENdw';
 
 	onMount(() => {
         var map = new mapboxgl.Map({
 			container: 'map',
-			style: 'mapbox://styles/mapbox/streets-v11'
+			style: 'mapbox://styles/mapbox/streets-v11',
+			center: [-74.5, 40],
+			zoom: 9
 		});
     });
 	
@@ -26,6 +29,7 @@
 </svelte:head>
 
 <div class="columns margins">
+	<div class="p-12 bg-red-200">test</div>
 	<div class="container">
 		<div class="card-container">
 			<Card>
@@ -44,9 +48,18 @@
 					/>
 					
 				</div>
-				<!-- <div id='map' style='width: 100%; height: 300px;'></div> -->
-				<Map></Map>
+
+				<div class="map" style='width: 100%; height: 300px;'>
+					<Map></Map>
+				</div>
+				
+				
 			</Card>
+
+			
+			
+
+			<!-- <div id='map' style='width: 100%; height: 300px;'></div> -->
 			
 		</div>
 	</div>
@@ -60,6 +73,11 @@
 	.card{
 		display: flex;
 		justify-content: space-between;
+		height: 100%;
+		width: 100%;
+	}
+	.map{
+		width: 100%;
 		height: 100%;
 	}
 	
