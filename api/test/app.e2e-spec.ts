7e264e5c8fe4461c4e9ca3d91296e0677181a6e8
Hugo Lavernhe <hugo.lavernhe@epitech.eu>
2022-06-09 17:06:28 +0200

    ✅ : unit & e2e tests

--- api/test/app.e2e-spec.ts
+++ api/test/app.e2e-spec.ts
@@ -1,24 +1,68 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import { AppModule } from './../src/app.module';
+import * as pactum from 'pactum';
+import { eachLike, like } from 'pactum-matchers';
 
 describe('AppController (e2e)', () => {
     let app: INestApplication;
 
-    beforeEach(async () => {
+    beforeAll(async () => {
         const moduleFixture: TestingModule = await Test.createTestingModule({
             imports: [AppModule],
         }).compile();
 
         app = moduleFixture.createNestApplication();
         await app.init();
+        await app.listen(3042);
+
+        pactum.request.setBaseUrl('http://localhost:3042');
     });
 
-    it('/ (GET)', () => {
-        return request(app.getHttpServer())
-            .get('/')
-            .expect(200)
-            .expect('Hello World!');
+    afterAll(() => {
+        app.close();
+    });
+
+    describe('Geo', () => {
+        describe('GET /geo/adresses/autocomplete?q=toulouse', () => {
+            it('should return an list of adresses', async () => {
+                await pactum
+                    .spec()
+                    .get('/geo/adresses/autocomplete?q=toulouse')
+                    .expectStatus(HttpStatus.OK)
+                    .expectJsonMatch(
+                        'content',
+                        eachLike({
+                            place_id: '321218857946',
+                            osm_id: '35738',
+                            osm_type: 'relation',
+                            licence: 'https://locationiq.com/attribution',
+                            lat: '43.6044622',
+                            lon: '1.4442469',
+                            boundingbox: [
+                                '43.532654',
+                                '43.668708',
+                                '1.3503956',
+                                '1.5153795',
+                            ],
+                            class: 'place',
+                            type: 'city',
+                            display_name:
+                                'Toulouse, Haute-Garonne, Occitanie, 31000;31100;31200;31300;31400;31500, France',
+                            display_place: 'Toulouse',
+                            display_address:
+                                'Haute-Garonne, Occitanie, 31000;31100;31200;31300;31400;31500, France',
+                            address: like({
+                                name: 'Toulouse',
+                                county: 'Haute-Garonne',
+                                state: 'Occitanie',
+                                postcode: '31000;31100;31200;31300;31400;31500',
+                                country: 'France',
+                                country_code: 'fr',
+                            }),
+                        }),
+                    );
+            });
+        });
     });
 });
