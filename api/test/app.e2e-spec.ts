c9dd318b3e236711afd2ddd49bee8db5b495ce78
Hugo Lavernhe <hugo.lavernhe@epitech.eu>
2022-06-17 17:28:33 +0200

    ✅ : e2e test

--- api/test/app.e2e-spec.ts
+++ api/test/app.e2e-spec.ts
@@ -1,9 +1,9 @@
 import { PrismaClient } from '@prisma/client';
+import { eachLike, like } from 'pactum-matchers';
 import { Test, TestingModule } from '@nestjs/testing';
 import { HttpStatus, INestApplication } from '@nestjs/common';
 import { AppModule } from './../src/app.module';
 import * as pactum from 'pactum';
-import { eachLike, like } from 'pactum-matchers';
 
 describe('AppController (e2e)', () => {
     let app: INestApplication;
@@ -35,29 +35,26 @@ describe('AppController (e2e)', () => {
                 ).resolves.not.toThrow();
             });
 
-            it('should signin', async () => {
-                return await pactum
+            it('should signin', () => {
+                return pactum
                     .spec()
                     .post('/auth/signin')
                     .withBody(dto)
                     .expectStatus(HttpStatus.ACCEPTED);
             });
 
-            it('should throw if no body provided', async () => {
-                return await pactum
+            it('should throw if no body provided', () => {
+                return pactum
                     .spec()
                     .post('/auth/signin')
                     .expectStatus(HttpStatus.BAD_REQUEST);
             });
 
-            it('should throw an 403 if credentials are not valids', async () => {
-                return await pactum
+            it('should throw an 403 if credentials are not valids', () => {
+                return pactum
                     .spec()
                     .post('/auth/signin')
-                    .withBody({
-                        email: `${EMAIL}aaaaaa`,
-                        password: `${PASSWORD}aaaa`,
-                    })
+                    .withBody({ email: 'test@test.com', password: 'password' })
                     .expectStatus(HttpStatus.FORBIDDEN);
             });
         });
@@ -65,8 +62,8 @@ describe('AppController (e2e)', () => {
 
     describe('Geo', () => {
         describe('GET /geo/adresses/autocomplete?q=toulouse', () => {
-            it('should return an list of adresses', async () => {
-                await pactum
+            it('should return an list of adresses', () => {
+                pactum
                     .spec()
                     .get('/geo/adresses/autocomplete?q=toulouse')
                     .expectStatus(HttpStatus.OK)
@@ -100,6 +97,55 @@ describe('AppController (e2e)', () => {
                                 country: 'France',
                                 country_code: 'fr',
                             }),
+                        }),
+                    );
+            });
+        });
+
+        describe('GET /geo/direction?latStart=43.603902&lngStart=1.440694&latEnd=44.842247&lngEnd=-0.569413', () => {
+            it('should return an direction as JSON', () => {
+                pactum
+                    .spec()
+                    .get(
+                        '/geo/direction?latStart=43.603902&lngStart=1.440694&latEnd=44.842247&lngEnd=-0.569413&profile=driving-car',
+                    )
+                    .expectStatus(HttpStatus.OK)
+                    .expectJsonMatch(
+                        'content.features[0].properties.segments[0].steps',
+                        eachLike({
+                            distance: 0.294,
+                            duration: 70.6,
+                            type: 0,
+                            instruction:
+                                'Tournez à gauche sur <b>Rue Peyrolières</b>',
+                            name: 'Rue Peyrolières',
+                            way_points: [10, 26],
+                        }),
+                    );
+            });
+        });
+
+        describe('GET /geo/places', () => {
+            it('should return places list as JSON', () => {
+                pactum
+                    .spec()
+                    .get(
+                        '/geo/places/eat?radius=10000&lat=43.604652&lng=1.444209',
+                    )
+                    .withRequestTimeout(180000)
+                    .expectStatus(HttpStatus.OK)
+                    .expectJsonMatch(
+                        'content',
+                        eachLike({
+                            type: 'node',
+                            id: 246587290,
+                            lat: 43.5748664,
+                            lon: 1.3745966,
+                            tags: {
+                                amenity: 'public_building',
+                                created_by: 'Potlatch 0.7',
+                                name: 'Meteo France',
+                            },
                         }),
                     );
             });
